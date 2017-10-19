ecdb8255e6663578c0e1f76e56b8037f2c405cd8
alien-house <info@alien-house.com>
2017-10-19 09:43:43 -0700

    add routing

--- tutorial/src/app/hero.service.ts
+++ tutorial/src/app/hero.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-/* At the moment, the AppComponent defines mock heroes for display. However, defining heroes is not the component's job, and you can't easily share the list of heroes with other components and views. In this page, you'll move the hero data acquisition business to a single service that provides the data and share that service with all components that need the data.*/
+import { Injectable } from '@angular/core';
+
 @Injectable()
-export class HeroService{
-    getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
-    }
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+               .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
